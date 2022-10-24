a9b5d4dd9e9a630f814a991a2229cd5664521d8c
william <452522426@qq.com>
2022-10-24 18:42:26 +0800

    版本升级 适配vb3.8.0

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,13 +1,12 @@
 object Versions {
-    const val compileSdkVersion = 30
+    const val compileSdkVersion = 33
     const val minSdkVersion = 21
-    const val targetSdkVersion = 30
-    const val buildToolsVersion = "29.0.2"
+    const val targetSdkVersion = 33
     const val versionCode = 100
     const val versionName = "1.0.0"
 }
 
-object Module {
+object MyModule {
     const val isDebug = true
     const val isLog = true
     const val isModuleRun = false
@@ -27,12 +26,10 @@ object Url {
 
 object Libs {
     val dependencies = arrayOf(
-        "com.github.oooo7777777:VLibrary:3.7.2",//VLibrary
+        "com.github.oooo7777777:VLibrary:3.8.0",//VLibrary
         "com.github.hackware1993:MagicIndicator:1.7.0",//指示器//
         "io.github.didi:drouter-api:2.1.3",//drouter
         "com.github.JessYanCoding:AndroidAutoSize:v1.2.1"//屏幕自适应
-
-
     )
 
 //    const val dependenciesKapt = arrayOf(
